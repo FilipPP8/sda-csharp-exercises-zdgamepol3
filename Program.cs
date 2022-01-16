7944783e9d4c34f538b0c2f8cfb0d4da5d583178
FilipPP8 <fparchanowicz@gmail.com>
2022-01-16 15:23:14 +0100

    Wykonano zadanie E04

--- Program.cs
+++ Program.cs
@@ -4,8 +4,34 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        static public void PrintTriangleAndReverse(char symbol, int number)
+        {
+            for (int i=0; i < number; i++)
+            {
+                for (int j=0; j <= i; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = number - 1; i > 0; i--)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj znak: ");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Podaj liczbę linii: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            PrintTriangleAndReverse(symbol, number);
         }
     }
 }
