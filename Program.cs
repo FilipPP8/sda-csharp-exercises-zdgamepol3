122c281c0df425e301ed3b324018c097ffc79b99
FilipPP8 <fparchanowicz@gmail.com>
2022-01-20 16:41:43 +0100

    Wykonano zadanie E08

--- Program.cs
+++ Program.cs
@@ -4,8 +4,33 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        static void printDistinct(int[] numbers)
+        {
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int j;
+
+                for (j = 0; j < i; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        break;
+                    }
+                }
+                
+                if (i == j)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+
+            }
+        }
         static void Main(string[] args)
         {
+            int[] numbers = { 0, 0, 2, 5, 1, 2, 5, 6, 0, 45, 321, 321, 45 };
+
+            printDistinct(numbers);
         }
     }
 }
