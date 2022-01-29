668bbb29383a46014d44d93cb0f46e00f7ab7ba6
FilipPP8 <fparchanowicz@gmail.com>
2022-01-29 16:16:37 +0100

    E20

--- Program.cs
+++ Program.cs
@@ -6,6 +6,30 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            Account account1 = new Account(5000);
+
+            SavingAccount account2 = new SavingAccount(10000, 200);
+
+            account1.IncreaseBalance(3000);
+
+            Console.WriteLine(account1.TotalBalance);
+
+            account2.IncreaseBalance(4000);
+
+            account2.IncreaseInterest(300);
+
+            Console.WriteLine(account2.TotalBalance);
+
+            Account[] accounts = { account1, account2 };
+
+            Bank bank = new Bank();
+
+            foreach (Account i in accounts)
+            {
+                bank.AddAccount(i);
+            }
+
+            bank.GetValue();
         }
     }
 }
---
+++ SavingAccount.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    public class SavingAccount : Account
+    {
+        public decimal interest { get; protected set; }
+
+        public SavingAccount(decimal balance, decimal interest)
+            : base(balance)
+        {
+            this.interest = interest;
+        }
+
+        public override decimal TotalBalance
+        {
+            get { return Balance + interest; }
+        }
+
+        public decimal IncreaseInterest(decimal increase)
+        {
+            return interest += increase;
+        }
+
+
+    }
+}
