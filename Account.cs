769b714b540bf824fff0ca317d591e5b197c7f97
FilipPP8 <fparchanowicz@gmail.com>
2022-01-29 15:46:09 +0100

    Wykonano zadanie E20

---
+++ Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    public class Account
+    {
+        public decimal Balance { get; protected set; }
+
+        public Account(decimal balance)
+        {
+            this.Balance = balance;
+        }
+
+        public decimal IncreaseBalance(decimal value)
+        {
+            return Balance += value;
+        }
+
+        public virtual decimal TotalBalance
+        {
+            get { return Balance; }
+        }
+
+    }
+}
---
+++ Bank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    public class Bank
+    {
+        List<Account> accounts = new List<Account>();
+
+        public void AddAccount(Account account)
+        {
+            accounts.Add(account);
+        }
+
+        public void GetValue()
+        {
+            decimal sum = 0;
+            foreach (Account a in accounts)
+            {
+                sum += a.TotalBalance;
+            }
+
+            Console.WriteLine("Łączna wartość wynosi: " + sum);
+        }
+    }
+}
