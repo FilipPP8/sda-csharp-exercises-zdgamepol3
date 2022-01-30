d0c21af015262519c235ee53d342c3263c6440b8
FilipPP8 <fparchanowicz@gmail.com>
2022-01-30 11:09:25 +0100

    Dołączono zmiany w ramach E32

--- Program.cs
+++ Program.cs
@@ -7,8 +7,18 @@ namespace sda_csharp_exercises
         static void Main(string[] args)
         {
             Rectangle rectangle1 = new Rectangle (4, 5);
+            Rectangle rectangle2 = new Rectangle(3, 2);
 
             Rectangle.PrintInfo(rectangle1);
+
+            Console.WriteLine("-----");
+
+            Rectangle[] rectangles = { rectangle1, rectangle2 };
+
+            foreach(Rectangle rectangle in rectangles)
+            {
+                Rectangle.PrintInfo(rectangle);
+            }
 
 
 
