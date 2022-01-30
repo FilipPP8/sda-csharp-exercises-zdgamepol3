600ac7d8b5520167db536ad8f4465bd7785c1486
FilipPP8 <fparchanowicz@gmail.com>
2022-01-30 11:51:42 +0100

    Wykonano E34

---
+++ Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Point
+    {
+        int X { get; set; }
+        int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static void Move(Point point, int moveX, int moveY)
+        {
+            point.X += moveX;
+            point.Y += moveY;
+        }
+
+        public static void ShowCurrentPosition(Point point)
+        {
+            Console.WriteLine("Aktualna pozycja punktu to " + point.X + " oraz " +point.Y);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,15 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            Point point = new Point(5, 4);
+
+            Point.ShowCurrentPosition(point);
+
+            Point.Move(point, 3, 2);
+
+            Point.ShowCurrentPosition(point);
+
+
         }
     }
 }
