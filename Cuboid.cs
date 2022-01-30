593ca7a1e32c106fbcdeab29337efb90fba83bf1
FilipPP8 <fparchanowicz@gmail.com>
2022-01-30 14:10:27 +0100

    Wykonano E37

---
+++ Cuboid.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Cuboid
+    {
+        double length;
+        double width;
+        double heigth;
+
+        public Cuboid(double length, double width, double heigth)
+        {
+            this.length = length;
+            this.width = width;
+            this.heigth = heigth;
+        }
+
+        public double GetVolume()
+        {
+            return length * width * heigth;
+        }
+
+        public static void CompareVolume(Cuboid a, Cuboid b)
+        {
+            if (a.GetVolume() > b.GetVolume())
+            {
+                Console.WriteLine("Większa objętość wynosi: " + a.GetVolume());
+            }
+            else if (b.GetVolume() > a.GetVolume())
+            {
+                Console.WriteLine("Większa objętość wynosi: " +b.GetVolume());
+            }
+            else
+            {
+                Console.WriteLine("Obie objętości są równe.");
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,23 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            Cuboid object1 = new Cuboid(3, 4, 5);
+            Cuboid object2 = new Cuboid(4, 5, 6);
+            Cuboid object3 = new Cuboid(3, 4, 5);
+
+            Console.WriteLine(object1.GetVolume());
+
+            Console.WriteLine(object2.GetVolume());
+
+            Cuboid.CompareVolume(object1, object2);
+
+            Console.WriteLine("------");
+
+            Console.WriteLine(object1.GetVolume());
+
+            Console.WriteLine(object3.GetVolume());
+
+            Cuboid.CompareVolume(object1, object3);
         }
     }
 }
