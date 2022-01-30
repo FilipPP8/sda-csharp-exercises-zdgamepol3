71b93c4efe38cd275dc499c9f9e713d93dab3fb3
FilipPP8 <fparchanowicz@gmail.com>
2022-01-30 10:19:07 +0100

    Wykonano zadanie E23

---
+++ LineSegment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class LineSegment
+    {
+        public MyPoint Start { get; protected set; }
+
+        public MyPoint End { get; protected set; }
+
+        public LineSegment(MyPoint start, MyPoint end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public LineSegment(int startX, int startY, int endX, int endY)
+        {
+            Start = new MyPoint(startX, startY);
+            End = new MyPoint(endX, endY);
+        }
+
+        public double GetLength()
+        {
+            int xA = Start.X;
+            int yA = Start.Y;
+            int xB = End.X;
+            int yB = End.Y;
+
+            double length = 0;
+
+            return length = Math.Sqrt(Math.Pow((xB - xA), 2) + Math.Pow((yB - yA),2));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            MyPoint punkt = new MyPoint(5, 7);
+            MyPoint punkt1 = new MyPoint(1, 3);
+
+            LineSegment linia1 = new LineSegment(punkt, punkt1);
+            LineSegment linia2 = new LineSegment(5, 3, 7, 8);
+
+            Console.WriteLine(linia1.GetLength());
+            Console.WriteLine(linia2.GetLength());
         }
     }
 }
